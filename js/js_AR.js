ce4595dc5bb35e5458c5cb057b2db2a9920b4dd0
adrianrl02 <124182523+adrianrl02@users.noreply.github.com>
2023-02-01 13:20:18 -0300

    Add files via upload

---
+++ js/js_AR.js
@@ -0,0 +1,5 @@
+
+document.getElementById('boton').onclick = function () {
+    console.log("elpepe");
+    document.getElementById('p1').innerHTML = " Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a   galley and scrambled it to make.";
+}
